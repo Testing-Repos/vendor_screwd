27686cf0b7331c0a73edd21d7eb83e30a77b6921
Abhiraj <abhirajsinghofficial@gmail.com>
2018-05-09 20:08:53 +0530

    Add maintainer

--- products/screwd_santoni.mk
+++ products/screwd_santoni.mk
@@ -11,6 +11,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H V9.5.4.0.NAMMIFA release-keys"
+    DEVICE_MAINTAINER="Abhiraj Singh"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/santoni/santoni:7.1.2/N2G47H/V9.5.4.0.NAMMIFA:user/release-keys"
